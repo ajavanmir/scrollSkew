e10b368ae575eb68f32f785ffb3a86f5464c27cb
amir javanmir <74532873+ajavanmir@users.noreply.github.com>
2024-04-24 14:53:29 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/*
+Copyright amir javanmir
+Released on: April 24, 2024
+*/
 let spanAngel = document.querySelector(".angle");
 window.addEventListener("scroll",function(){
     let value = -10 + window.scrollY/60;
